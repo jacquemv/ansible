894463ed73553406cea462db65c5fd56838356a8
Vincent Jacquemet <vincent.jacquemet@umontreal.ca>
2022-07-06 11:49:40 -0400

    update anaconda fix

--- howto/fix_anaconda2019.03_F30.sh
+++
@@ -1,5 +0,0 @@
-# pyopengl requires GLIBCXX_3.4.26
-rm /opt/anaconda3/lib/libstdc++.so
-rm /opt/anaconda3/lib/libstdc++.so.6
-ln -s /usr/lib64/libstdc++.so.6 /opt/anaconda3/lib/libstdc++.so.6
-ln -s /opt/anaconda3/lib/libstdc++.so.6 /opt/anaconda3/lib/libstdc++.so
